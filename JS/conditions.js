f9d10ea7d250c9cc500de459a9f96dacd8ed7de7
Gabriel Lourenço <75556848+lourenco2@users.noreply.github.com>
2021-08-09 16:37:36 -0300

    Add files via upload

---
+++ JS/conditions.js
@@ -0,0 +1,10 @@
+
+//Codições
+
+       if (true){
+        document.write('bloco IF(verdadeiro)')
+       } 
+       else{
+        document.write('bloco ELSE (falso)')
+       }    
+    
---
+++ JS/variables.js
@@ -0,0 +1,6 @@
+
+//Variaveis
+
+var nome = prompt('Digite o seu nome:')
+var idade = prompt('Digite a sua idade:')
+document.write('<h1>Boa noite '+nome+', tudo bem? Estou vendo aqui que você possui '+idade+' anos.</h1>')
